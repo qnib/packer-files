e88257f38cd2491c40b88cbbd23fe30582a9b810
Christian Kniep <christian@qnib.org>
2017-11-24 16:33:24 -0800

    update doxy

--- aws-u16-gpu/scripts/doxy.sh
+++ aws-u16-gpu/scripts/doxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -qO /usr/bin/doxy https://github.com/qnib/doxy/releases/download/v0.2.1/doxy_x86_v0.2.1
+wget -qO /usr/bin/doxy https://github.com/qnib/doxy/releases/download/v0.2.3/doxy_x86_v0.2.3
 chmod +x /usr/bin/doxy
 ## SystemD
 mv /home/ubuntu/doxy.service /lib/systemd/system/
