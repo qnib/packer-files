9963d2f08903d0df251f5a8958cf6d9cae92b813
Christian Kniep <christian@qnib.org>
2017-11-26 09:40:01 -0800

    pin /etc/hosts to terraform-script/aws/slurm

--- aws-u16-slurm/files/write-slurm-conf.sh
+++ aws-u16-slurm/files/write-slurm-conf.sh
@@ -9,8 +9,20 @@ systemctl stop slurmctld
 sleep 1
 echo ">> Run `confd -onetime -backend env`"
 confd -onetime -backend env
+
+if [[ -f /home/ubuntu/etc/hosts.new ]];then
+  echo ">> Add fixed set of hosts to /etc/hosts"
+  echo "" >> /etc/hosts
+  echo "## Fixed list of hosts to allow slurm-cluster to form" >> /etc/hosts
+  cat /home/ubuntu/etc/hosts.new
+  cat /home/ubuntu/etc/hosts.new >> /etc/hosts
+  #rm -f /home/ubuntu/etc/hosts.new
+fi
+sleep 1
 echo ">> Start slurmctld/slurmd"
-systemctl start slurmctld
+if [[ $(hostname) == "master0" ]];then
+  systemctl start slurmctld
+fi
 systemctl start slurmd
 
 echo ">> Restart doxy for good measure"
--- aws-u16-slurm/scripts/mkdirs.sh
+++ aws-u16-slurm/scripts/mkdirs.sh
@@ -2,5 +2,5 @@
 set -x
 
 mkdir -p /home/ubuntu/confd/{conf.d,templates}
-mkdir -p /home/ubuntu/{slurm,systemd}
-chown -R ubuntu: /home/ubuntu/{confd,systemd,slurm}
+mkdir -p /home/ubuntu/{slurm,systemd,etc,bin}
+chown -R ubuntu: /home/ubuntu/{confd,systemd,slurm,etc,bin}
