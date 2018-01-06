5a43f4e2d3040cdf19ab704991ac59d7639ba237
Christian Kniep <christian@qnib.org>
2018-01-06 18:46:55 +0100

    add qlpine-docker vbox

--- alpine-docker/provisioners/vagrant.sh
+++ alpine-docker/provisioners/vagrant.sh
@@ -2,7 +2,7 @@
 (echo $VAGRANT_PASSWORD; echo $VAGRANT_PASSWORD) | adduser -s /bin/ash $VAGRANT_USERNAME
 mkdir -pm 700 /home/$VAGRANT_USERNAME/.ssh
 wget -O - 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub' >> /home/$VAGRANT_USERNAME/.ssh/authorized_keys
-chown -R $VAGRANT_USERNAME:$VAGRANT_USERNAME /home/$VAGRANT_USERNAME     
+chown -R $VAGRANT_USERNAME:$VAGRANT_USERNAME /home/$VAGRANT_USERNAME
 chmod -R og-rwx /home/$VAGRANT_USERNAME/.ssh
 echo "$VAGRANT_USERNAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$VAGRANT_USERNAME
 cat > /usr/sbin/shutdown << 'EOF'
@@ -16,3 +16,4 @@ EOF
 chmod a+rx /usr/sbin/shutdown
 apk add nfs-utils
 apk add rsync
+apk add bash
