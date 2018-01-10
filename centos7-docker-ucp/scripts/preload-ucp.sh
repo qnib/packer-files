4ea5036943eeec6b625700632388664f93a9b914
Christian Kniep <christian@qnib.org>
2018-01-10 12:29:11 +0100

    adjust preload-ucp to be flexible

--- centos7-docker-ucp/scripts/preload-ucp.sh
+++ centos7-docker-ucp/scripts/preload-ucp.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-: ${UCP_VERSION:=2.2.4}
+UCP_VERSION=${1}
+
+if [[ "X${UCP_VERSION}" == "X" ]];then
+  echo ">> Please provide UCP_VERSION as argument: ./${0} 2.2.4"
+  exit 1
+fi
 
 echo "Downloading docker/ucp:${UCP_VERSION}"
 docker pull docker/ucp:${UCP_VERSION}
 
-for SUB_IMG in agent auth auth-store cfssl compose controller dsinfo etcd hrm metrics swarm;do
+for SUB_IMG in auth hyperkube swarm calico-cni dsinfo interlock-proxy kube-compose calico-kube-controllers etcd agent kube-dns-dnsmasq-nanny cfssl kube-dns-sidecar controller compose pause interlock-extension auth-store kube-dns metrics calico-node interlock;do
   echo "Downloading docker/ucp-${SUB_IMG}:${UCP_VERSION}"
   docker pull docker/ucp-${SUB_IMG}:${UCP_VERSION}
 done
