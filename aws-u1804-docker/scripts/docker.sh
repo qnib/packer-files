f6993f4a6ad6a15380a1857abf327cbb3e21ff6b
Christian Kniep <christian@qnib.org>
2019-07-11 16:07:36 +0200

    initial commit

---
+++ aws-u1804-docker/scripts/docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+rm -f /var/lib/dpkg/lock
+apt-get install -y --allow-unauthenticated docker-ce
