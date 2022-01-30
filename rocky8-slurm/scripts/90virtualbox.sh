b2d69afc3d92b086b38fc4554a0424ddd070dde8
Christian Kniep <christian@qnib.org>
2022-01-30 20:47:53 +0000

    add virtualbox script

---
+++ rocky8-slurm/scripts/90virtualbox.sh
@@ -0,0 +1,21 @@
+set -ux
+
+if [ "$guest_additions_mode" = "disable" ]; then
+    exit 0
+fi
+
+#
+# packages need to install VBGA
+#
+dnf -y install bzip2 elfutils-libelf-devel epel-release gcc kernel-devel-`uname -r` kernel-headers perl tar 
+dnf -y install dkms
+
+VBOX_VERSION=$(cat /home/vagrant/.vbox_version)
+cd /tmp
+mount -o loop /home/vagrant/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+sh /mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf /home/vagrant/VBoxGuestAdditions_*.iso
+systemctl enable vboxadd.service
+
+    
